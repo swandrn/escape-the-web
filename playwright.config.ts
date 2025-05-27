77dfd069e788b461591f2a3f22690358746a233f
swandrn <159421702+swandrn@users.noreply.github.com>
2025-05-27 11:03:15 +0200

    ops: run linter and test on push

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,9 +1,14 @@
 import { defineConfig } from "@playwright/test";
 
 export default defineConfig({
+  webServer: {
+    command: "npm run dev",
+    port: 5173,
+    reuseExistingServer: !process.env.CI,
+  },
   use: {
     baseURL: "http://localhost:5173",
-    headless: false,
+    headless: true,
   },
-  testDir: "src/tests",
+  testDir: "src/tests/e2e",
 });
--- vite.config.ts
+++ vite.config.ts
@@ -8,5 +8,13 @@ export default defineConfig({
     globals: true,
     environment: "jsdom",
     setupFiles: "./src/setupTests.ts",
+    exclude: [
+      "**/node_modules/**",
+      "**/dist/**",
+      "**/cypress/**",
+      "**/.{idea,git,cache,output,temp}/**",
+      "**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build,eslint,prettier}.config.*",
+      "./src/tests/e2e/**",
+    ],
   },
 });
